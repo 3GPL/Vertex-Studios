3f43732ced5402a3ee47355bed4634a27c40e9c0
CapsLock <jiyong.lol6342@gmail.com>
2024-10-29 20:43:01 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript for future functionality
+document.querySelector('.dynamic-island').addEventListener('mouseover', () => {
+    console.log('Dynamic Island hovered');
+});
+
+document.querySelector('.dynamic-island').addEventListener('mouseout', () => {
+    console.log('Dynamic Island unhovered');
+});
